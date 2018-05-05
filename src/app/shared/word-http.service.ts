a232daccbda08e82de5183061ab710976748dddd
kmhoran <kevin.michael.horan@gmail.com>
2018-05-05 10:04:34 -0700

    ... hmmm messa round a bit with services and look into creating an interceptor.. Oh well, VRLA today!

--- src/app/shared/word-http.service.ts
+++ src/app/shared/word-http.service.ts
@@ -1,33 +1,16 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs/Rx';
 
 import { IWord } from './iword';
 
 @Injectable()
 export class WordHttpService {
-  url = 'https://127.0.0.1:8000/api/word/';
+  url = 'http://127.0.0.1:8000/api/word/';
   constructor(private _http: HttpClient) { }
 
-  getWords(): IWord[] {
-    return [
-      {
-        id: 1,
-        name: 'one',
-        definitions: null
-      },
-      {
-        id: 2,
-        name: 'two',
-        definitions: null
-      },
-    ];
+  getWords(): Observable<IWord[]> {
+    return this._http.get<IWord[]>(this.url);
   }
-
-
-
-
-getTEST() {
-  return this._http.get(this.url);
-}
 }
